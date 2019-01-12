d0cab2b62e1be587a5253bd48e2fe36081700608
Red Fox <sarahanne118@gmail.com>
2019-01-12 13:01:25 -0700

    fixing bugs

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(router);
 
 var CONNECTION_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
 
-mongoose.connect(CONNECTION_URI, { useMongoClient: true }).then(() => {
+mongoose.connect(CONNECTION_URI, { MONGODB_URI: true }).then(() => {
     console.log('Connected to MongoDB.');
 }).catch(err => console.log(err));
 
